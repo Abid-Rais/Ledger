28829fa7563675eb48431def4187881c04497f4d
Abid-Rais <abidrais1843@gmail.com>
2021-04-28 01:33:18 -0400

    Created login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,48 @@
+import React, { ReactElement } from 'react';
+
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+const Login: React.FC = (): ReactElement => {
+    const onFinish = () => {
+        return null;
+    };
+
+    return (
+        <Form name="Login" className="login-form" onFinish={onFinish}>
+            <Form.Item
+                name="username"
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your Username',
+                    },
+                ]}
+            >
+                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            </Form.Item>
+
+            <Form.Item name="Password" rules={[{ required: true, message: 'Please input your Password' }]}>
+                <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                />
+            </Form.Item>
+
+            <Form.Item>
+                <a className="login-form-forgot" href="">
+                    Forgot password
+                </a>
+            </Form.Item>
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Log in
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+};
+
+export default Login;
